7223d59a6a23d5afe0db008b4e16300c21bffded
vizzie <ed@vizzie.com>
2019-09-23 20:41:45 -0400

    added these to get it to build on idf

--- include/ableton/platforms/esp32/ESP32.hpp
+++ include/ableton/platforms/esp32/ESP32.hpp
@@ -10,3 +10,7 @@
 #ifndef htonll
 #define htonll(x) __bswap_64(x)
 #endif
+
+//dummy definitions to get this to build
+//static char* if_indextoname(unsigned int , char* ) { return 0; }
+//static unsigned int if_nametoindex(const char*) {return 0; }
--- include/ableton/platforms/esp32/ScanIpIfAddrs.hpp
+++ include/ableton/platforms/esp32/ScanIpIfAddrs.hpp
@@ -4,9 +4,9 @@
 
 #include <ableton/platforms/asio/AsioWrapper.hpp>
 #include <ableton/platforms/asio/Util.hpp>
-#include <arpa/inet.h>
-#include <net/if.h>
-#include <vector>
+// #include <arpa/inet.h>
+// #include <net/if.h>
+//#include <vector>
 #include <tcpip_adapter.h>
 
 namespace ableton
@@ -27,11 +27,11 @@ struct ScanIpIfAddrs
     tcpip_adapter_ip_info_t info;
 
     auto err = tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &info);
-    if (err == ESP_OK) 
+    if (err == ESP_OK)
     {
       addrs.emplace_back(asio::makeAddress<::asio::ip::address_v4>(ip4addr_ntoa(&info.ip)));
     }
-    
+
     err = tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_AP, &info);
     if (err == ESP_OK)
     {
